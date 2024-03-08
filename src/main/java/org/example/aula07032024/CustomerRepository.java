f4ca5553eb2f2d29aa9fd39a39758eda924937b4
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-03-07 21:51:23 -0300

    Primeiro commit

---
+++ src/main/java/org/example/aula07032024/CustomerRepository.java
@@ -0,0 +1,12 @@
+package org.example.aula07032024;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    List<Customer> findByLastName(String lastName);
+
+    Customer findById(long id);
+}
---
+++ src/test/java/org/example/aula07032024/Aula07032024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aula07032024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula07032024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
